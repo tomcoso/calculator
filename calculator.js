3e455f27b7472647129f435af7e0d9b0411b1cbf
tomcoso <tomasdessy0@gmail.com>
2022-04-24 19:08:06 +0100

    integrate basic features

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ let current = {
 }
 
 function operate(newOperator) {
+    let result;
     switch (current.operator) {
         case '+' :
             current.firstOperand = +current.firstOperand + +current.secondOperand;
@@ -12,7 +13,7 @@ function operate(newOperator) {
         case '-' :
             current.firstOperand = +current.firstOperand - +current.secondOperand;
             break;
-        case '*' :
+        case 'x' :
             if (current.secondOperand == '0') {
                 current.firstOperand = '0';
                 break;
@@ -30,5 +31,71 @@ function operate(newOperator) {
         current.operator = null;
     }
 
+    if (current.operator) 
+    {
+        displayTop.textContent = `${current.firstOperand} ${current.operator}`;
+        displayBottom.textContent = '0';
+    }
+    else
+    {
+        displayTop.textContent += ` ${current.secondOperand}`;
+        displayBottom.textContent = current.firstOperand;
+        current.use = true; //so that user can start from zero without using clear
+    }
+
     current.secondOperand = null;
 }
+
+//DISPLAY
+const displayBottom = document.querySelector('.display.bottom');
+const displayTop = document.querySelector('.display.top');
+
+//OPERANDS
+const numbers = document.querySelectorAll('.number');
+numbers.forEach(number => number.addEventListener('click', event => {
+
+    if (current.use) 
+    {
+        current.firstOperand = null;
+        delete current.use;
+        displayBottom.textContent = event.target.textContent;
+    }
+    else if (displayBottom.textContent == '0') 
+    {
+        displayBottom.textContent = event.target.textContent;
+    } 
+    else 
+    {
+        displayBottom.textContent += event.target.textContent;
+    }
+}))
+
+//OPERATORS
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => operator.addEventListener('click', event => {
+
+    if (current.operator) 
+    {
+        current.secondOperand = displayBottom.textContent;
+        operate(event.target.textContent);
+
+    }
+     else 
+     {
+        if (event.target.textContent == '=') return;
+
+        if (event.target.textContent == '-') 
+        {
+            displayBottom.textContent = '-' ;
+            return;
+        }
+
+        current.firstOperand = displayBottom.textContent;
+        current.operator = event.target.textContent;
+
+        displayTop.textContent = (`${current.firstOperand} ${current.operator}`)
+        displayBottom.textContent = '0';
+
+        if (current.use) delete current.use;
+    }
+}))
