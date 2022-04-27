14cce9c4c3428a01bbe1ca0d51e731dd1bc8aecc
tomcoso <tomasdessy0@gmail.com>
2022-04-27 18:49:58 +0100

    add keyboard integration and sound effects

--- calculator.js
+++ calculator.js
@@ -25,9 +25,16 @@ function operate(newOperator) {
             break;
     }
 
-    if (newOperator != '=') {
+    if (newOperator != '=') 
+    {
         current.operator = newOperator;
-    } else {
+    }
+    else if (newOperator == 'Enter')
+    {
+        current.operator = null;
+    }
+    else
+    {
         current.operator = null;
     }
 
@@ -68,7 +75,8 @@ const displayTop = document.querySelector('.display.top');
 
 //OPERANDS
 const numbers = document.querySelectorAll('.number');
-numbers.forEach(number => number.addEventListener('click', event => {
+numbers.forEach(number => number.addEventListener('click', addOperand)) ;
+function addOperand(event) {
 
     if (current.use) 
     {
@@ -84,11 +92,16 @@ numbers.forEach(number => number.addEventListener('click', event => {
     {
         displayBottom.textContent += event.target.textContent;
     }
-}))
+
+    let audio = new Audio('buttonsound.wav');
+    audio.play();
+
+}
 
 //OPERATORS
 const operators = document.querySelectorAll('.operator');
-operators.forEach(operator => operator.addEventListener('click', event => {
+operators.forEach(operator => operator.addEventListener('click', addOperator)) ;
+function addOperator(event) {
 
     if (current.operator) 
     {
@@ -116,11 +129,16 @@ operators.forEach(operator => operator.addEventListener('click', event => {
         if (current.use) delete current.use;
         if (current.float) delete current.float;
     }
-}))
+
+    let audio = new Audio('specialsound.wav');
+    audio.play();
+
+}
 
 //SPECIALS
 const floatBtn = document.querySelector('.float');
-floatBtn.addEventListener('click', () => {
+floatBtn.addEventListener('click', addFloat) ;
+function addFloat() {
 
     if (current.float) // So that we can only use one float per operand
     {
@@ -142,11 +160,14 @@ floatBtn.addEventListener('click', () => {
     }
 
     current.float = true
+    let audio = new Audio('buttonsound.wav');
+    audio.play();
 
-})
+}
 
 const clearBtn = document.querySelector('.clear');
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener('click', clear);
+function clear() {
 
     displayBottom.textContent = '0' ;
     displayTop.textContent = '' ;
@@ -158,13 +179,22 @@ clearBtn.addEventListener('click', () => {
     if (current.use) delete current.use;
     if (current.float) delete current.float;
 
-})
+    let audio = new Audio('clearsound.wav');
+    audio.play();
+
+}
 
 const delBtn = document.querySelector('.delete');
-delBtn.addEventListener('click', () => {
+delBtn.addEventListener('click', deleteLast)
+function deleteLast() {
+
     let str = displayBottom.textContent ;
 
     if (str.length === 1) 
+    {
+        displayBottom.textContent = '0' ;
+    }
+    else if (current.use)
     {
         displayBottom.textContent = '0' ;
     }
@@ -173,4 +203,103 @@ delBtn.addEventListener('click', () => {
         displayBottom.textContent = str.substring(0, str.length-1);
     }
 
+    let audio = new Audio('specialsound.wav');
+    audio.play();
+
+}
+
+//KEYBOARD INTEGRATION
+window.addEventListener('keydown', (event) => {
+    console.log(event);
+
+    if (event.key >= 0 && event.key <= 9 && event.key != ' ') 
+    {
+        addOperandKey(event);
+    }
+    else if (
+        event.key == '+' ||
+        event.key == '-' ||
+        event.key == '*' ||
+        event.key == '/' ||
+        event.key == 'Enter')
+    {
+        addOperatorKey(event);
+    }
+    else if (event.key == 'Backspace')
+    {
+        deleteLast();
+    }
+    else if (event.key == '.')
+    {
+        addFloat();
+    }
+    else if (event.code == 'Space')
+    {
+        event.preventDefault();
+        clear();
+    }
 })
+
+function addOperandKey(event) {
+
+    if (current.use) 
+    {
+        current.firstOperand = null;
+        delete current.use;
+        displayBottom.textContent = event.key;
+    }
+    else if (displayBottom.textContent == '0') 
+    {
+        displayBottom.textContent = event.key;
+    } 
+    else 
+    {
+        displayBottom.textContent += event.key;
+    }
+
+    let audio = new Audio('buttonsound.wav');
+    audio.play();
+}
+
+function addOperatorKey(event) {
+
+    let operator;
+    if (event.key == 'Enter')
+    {
+        operator = '=' ;
+    }
+    else 
+    {
+        operator = event.key;
+    }
+
+    if (current.operator) 
+    {
+        current.secondOperand = displayBottom.textContent;
+        operate(operator);
+
+        if (current.float) delete current.float;
+    }
+     else 
+    {
+        if (operator == '=') return;
+
+        if (operator == '-' && displayBottom.textContent == '0') 
+        {
+            displayBottom.textContent = '-' ;
+            return;
+        }
+
+        current.firstOperand = displayBottom.textContent;
+        current.operator = operator;
+
+        displayTop.textContent = (`${current.firstOperand} ${current.operator}`)
+        displayBottom.textContent = '0';
+
+        if (current.use) delete current.use;
+        if (current.float) delete current.float;
+    }
+
+    let audio = new Audio('specialsound.wav');
+    audio.play();
+}
